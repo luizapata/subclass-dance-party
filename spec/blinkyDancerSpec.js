4816811d162d880e90d21b5db7fe9a496b0747f2
luizapata <lufeza95@gmail.com>
2016-06-27 20:09:54 -0700

    one test passing

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -6,7 +6,8 @@ describe("blinkyDancer", function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
+    console.dir(blinkyDancer)
   });
 
   it("should have a jQuery $node object", function(){
@@ -15,15 +16,17 @@ describe("blinkyDancer", function() {
 
   it("should have a step function that makes its node blink", function() {
     sinon.spy(blinkyDancer.$node, 'toggle');
+    //console.log(blinkyDancer.$node)
     blinkyDancer.step();
     expect(blinkyDancer.$node.toggle.called).to.be.true;
+    console.log(blinkyDancer.$node.toggle.called)
   });
 
   describe("dance", function(){
     it("should call step at least once per second", function(){
       sinon.spy(blinkyDancer, "step");
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+      blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
       clock.tick(timeBetweenSteps);
 
       expect(blinkyDancer.step.callCount).to.be.equal(1);
--- src/dancer.js
+++ src/dancer.js
@@ -1,33 +1,38 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps){
 
-  var dancer = {};
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  $node = $('<span class="dancer"></span>');
+  this.$node = $node;
 
 
-  dancer.step = function(){
+  this.constructor.prototype.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
 
-  dancer.setPosition = function(top, left){
+    setTimeout(this.constructor.prototype.step.bind(this.$node), timeBetweenSteps);
+    console.log(this.constructor.prototype.step)
+  };
+  //this.step();
+
+  this.constructor.prototype.setPosition = function(top, left){
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
+
     var styleSettings = {
       top: top,
       left: left
     };
-    dancer.$node.css(styleSettings);
+
+    this.$node.css('styleSettings');
+
   };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  //dancer.setPosition(top, left);
 
-  return dancer;
 };
+console.dir(new makeDancer());
